ad88444ff6ddef9ce5d421c98939e2e724bdbe66
pierzbarry <GBTC406007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-07 16:12:28 -0400

    new commit

---
+++ src/main/java/com/example/demo/Exercise204bApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise204bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise204bApplication.class, args);
+    }
+
+}
